9f0c1e899e0da8187f5b4435bb5945df4a6f74a9
Gary <changary1994@gmail.com>
2022-04-09 18:18:41 -0400

    complete progress bar script

--- Assets/ProgressBar.cs
+++ Assets/ProgressBar.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ProgressBar : MonoBehaviour
+using UnityEngine.Events;
+public class ProgressBar : FillBar
 {
+    // Event to invoke when progress bar fills up
+    private UnityEvent onProgressComplete;
+
+    // Create a property to handle the slider's value
+    public new float CurrentValue
+    {
+        get
+        {
+            return base.CurrentValue;
+        }
+        set
+        {
+            // If the value exceeds the max fill, invoke the completion function
+            if (value >= slider.maxValue)
+                onProgressComplete.Invoke();
+            // Remove any overfill
+            base.CurrentValue = value % slider.maxValue;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (onProgressComplete == null)
+            onProgressComplete = new UnityEvent();
+        onProgressComplete.AddListener(OnProgressComplete);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CurrentValue += 0.00153f;
+    }
+
+    void OnProgressComplete()
+    {
+        Debug.Log("Progress Complete");
     }
 }
