0acc7d159c48477d8ee26a0364b4067992a59250
Cory Campbell <Cory Campbell>
2022-04-13 22:43:33 -0400

    Trivia and Question component integrated

---
+++ Assets/LetterSlot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LetterSlot : MonoBehaviour
+{
+    public int slotIndex;
+    public bool occupied = false;
+}
---
+++ Assets/TriviaTracker.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriviaTracker : MonoBehaviour
+{
+    [SerializeField] public static int currentQuestion;
+}
