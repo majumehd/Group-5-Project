cee8c6c88d1b158b2e8a2a5dae8320b8d0da49bf
Cory Campbell <Cory Campbell>
2022-05-01 17:52:02 -0400

    Update Folder management and Scene Loader

--- Assets/TriviaTracker.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TriviaTracker : MonoBehaviour
-{
-    [SerializeField] public static int currentQuestion;
-}
