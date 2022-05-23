86a39bc51213316fcd738006e625d79b1bee59c0
Mehdi Majumder <mehdi.h.majumder@gmail.com>
2022-05-23 11:41:17 -0400

    added lives

--- Assets/Scripts/AnswerHandler.cs
+++ Assets/Scripts/AnswerHandler.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class AnswerHandler : MonoBehaviour
 {
+    public GameObject heart1, heart2, heart3, heart4, heart5, gameOver;
+    public static int heart;
     Text inputField;
     public static string input;
     public static bool finish = false;
@@ -15,13 +17,73 @@ public class AnswerHandler : MonoBehaviour
     public Text incorrectTxt;
     void Start()
     {
-       if(inputField == null)
+       heart = 5;
+       heart1.gameObject.SetActive(true);
+       heart2.gameObject.SetActive(true);
+       heart1.gameObject.SetActive(true);
+       heart1.gameObject.SetActive(true);
+       heart1.gameObject.SetActive(true);
+       gameOver.gameObject.SetActive(false);
+
+        if (inputField == null)
         inputField = GameObject.FindGameObjectWithTag("input").GetComponent<Text>();
+
 
     }
 
     void Update()
     {
+        if (heart > 5)
+            heart = 5;
+        switch (heart)
+        {
+            case 5:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(true);
+                heart5.gameObject.SetActive(true);
+                break;
+            case 4:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(true);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 3:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 2:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(false);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                break;
+            case 0:
+                heart1.gameObject.SetActive(false);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                heart4.gameObject.SetActive(false);
+                heart5.gameObject.SetActive(false);
+                Time.timeScale = 3f;
+                gameOver.gameObject.SetActive(true);
+                SceneManager.LoadScene("LevelSelect");
+                break;
+
+        }
        /* if (finish && !win)
         {
             Debug.Log(input);
@@ -65,6 +127,7 @@ public class AnswerHandler : MonoBehaviour
                 finish = false;
                 answerInput.text = "";
                 incorrectTxt.StartCoroutine(FadeIncorrect());
+                decreaseHearts();
 
             }
         }
@@ -116,5 +179,11 @@ public class AnswerHandler : MonoBehaviour
             incorrectTxt.color = new Color(1, 1, 1, i);
             yield return null;
         }
+        
+    }
+
+    public void decreaseHearts()
+    {
+        heart -= 1;
     }
 }
